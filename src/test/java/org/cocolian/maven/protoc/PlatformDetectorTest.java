3f05573cbe5ce45a32bfe39c1518c83843a169f6
fqyy365t@hotmail.com <test123>
2018-05-04 09:27:16 +0800

    新增PlatformDetectorTest测试案例

--- src/test/java/org/cocolian/maven/protoc/PlatformDetectorTest.java
+++ src/test/java/org/cocolian/maven/protoc/PlatformDetectorTest.java
@@ -1,14 +1,33 @@
 package org.cocolian.maven.protoc;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlatformDetectorTest
 {
+  PlatformDetector platform;
+
+  @Before
+  public void setUp() throws Exception
+  {
+    platform = new BasicPlatformDetector();
+  }
+
+  /**
+   * 获取操作系统分类
+   */
+  @Test
+  public void normalizeOsTest()
+  {
+    {
+      String normalizeOs = platform.normalizeOs();
+      System.out.printf("操作系统信息：%s", normalizeOs);
+    }
+  }
   @Test
   public void getClassfierTest()
   {
     {
-      PlatformDetector platform = new BasicPlatformDetector();
       String classfier = platform.getClassfier();
       System.out.printf("平台信息：%s", classfier);
     }
