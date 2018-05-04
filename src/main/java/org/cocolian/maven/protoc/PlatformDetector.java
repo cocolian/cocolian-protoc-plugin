21e5f4b22e6a9fa632c14368e422495a7a337157
fqyy365t@hotmail.com <test123>
2018-05-04 09:03:58 +0800

    新增获取操作系统和指令集方法

--- src/main/java/org/cocolian/maven/protoc/PlatformDetector.java
+++ src/main/java/org/cocolian/maven/protoc/PlatformDetector.java
@@ -1,6 +1,31 @@
 package org.cocolian.maven.protoc;
 
+/**
+ * 平台探测器
+ * 
+ * @author 何阳
+ *
+ */
 public interface PlatformDetector
 {
-    String getClassfier();
+  /**
+   * 获取操作系统分类
+   * 
+   * @return String
+   */
+  public String getClassfier();
+
+  /**
+   * 获取标准化的操作系统信息
+   * 
+   * @return String
+   */
+  public String normalizeOs();
+
+  /**
+   * 获取标准化的操作系统指令集
+   * 
+   * @return String
+   */
+  public String normalizeArch();
 }
